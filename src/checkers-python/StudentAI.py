87f12d4333252704945de4bea710e4dd9aff9e6c
Ethan Louie <eklouie@uci.edu>
2020-11-13 16:55:06 -0800

    fixed example AI files

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -22,6 +22,7 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
+        
     def get_move(self,move):
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
@@ -34,45 +35,39 @@ class StudentAI():
         self.board.make_move(move,self.color)
         return move
 
+    def mcts(root): 
+            while resources_left(time, computational power): 
+                    leaf = traverse(root) 
+                    simulation_result = rollout(leaf) 
+                    backpropagate(leaf, simulation_result) 
+                    
+            return best_child(root) 
 
+    # function for node traversal 
+    def traverse(node): 
+            while fully_expanded(node): 
+                    node = best_uct(node) 
+                    
+            # in case no children are present / node is terminal 
+            return pick_univisted(node.children) or node 
 
+    # function for the result of the simulation 
+    def rollout(node): 
+            while non_terminal(node): 
+                    node = rollout_policy(node) 
+            return result(node) 
 
+    # function for randomly selecting a child node 
+    def rollout_policy(node): 
+            return pick_random(node.children) 
 
-### main function for the Monte Carlo Tree Search 
-##def monte_carlo_tree_search(root): 
-##	
-##	while resources_left(time, computational power): 
-##		leaf = traverse(root) 
-##		simulation_result = rollout(leaf) 
-##		backpropagate(leaf, simulation_result) 
-##		
-##	return best_child(root) 
-##
-### function for node traversal 
-##def traverse(node): 
-##	while fully_expanded(node): 
-##		node = best_uct(node) 
-##		
-##	# in case no children are present / node is terminal 
-##	return pick_univisted(node.children) or node 
-##
-### function for the result of the simulation 
-##def rollout(node): 
-##	while non_terminal(node): 
-##		node = rollout_policy(node) 
-##	return result(node) 
-##
-### function for randomly selecting a child node 
-##def rollout_policy(node): 
-##	return pick_random(node.children) 
-##
-### function for backpropagation 
-##def backpropagate(node, result): 
-##	if is_root(node) return
-##	node.stats = update_stats(node, result) 
-##	backpropagate(node.parent) 
-##
-### function for selecting the best child 
-### node with highest number of visits 
-##def best_child(node): 
-##	pick child with highest number of visits 
+    # function for backpropagation 
+    def backpropagate(node, result): 
+            if is_root(node) return
+            node.stats = update_stats(node, result) 
+            backpropagate(node.parent) 
+
+    # function for selecting the best child 
+    # node with highest number of visits 
+    def best_child(node): 
+            pick child with highest number of visits 
