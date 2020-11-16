4fa2f80f952860b85cd02aa03420fc21e95019f0
Ethan Louie <eklouie@uci.edu>
2020-11-15 18:35:13 -0800

    major refractoring

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -27,24 +27,34 @@ from copy import deepcopy
 class GameTreeNode():
     opponent = {1:2, 2:1}
 
-    def __init__(self, board, color, move):
-#         self.children = list()
-#         self.parent = None
+    def __init__(self, board, color, move, parent):
 #         self.ties = 0 # consider ties as half a win for grading (not for tournament)?
-        self.board = board
+        self.parent = parent
         self.color = color
         self.move = move
         self.visit_count = 0
         self.wins = 0
         self.win_rate = 0
+        self.board = deepcopy(board)
+        # execute child nodes' first move
+        if self.parent != None:
+            temp_board.make_move(self.move, self.color)
+            
+    def create_children(self):
+        '''
+        Create new nodes for all possible moves
+        '''
+        self.children = list()
+        moves_list = self.board.get_all_possible_moves(self.color)
+        for i in range(len(moves_list)):
+            for j in range(len(moves_list[i])):
+                self.children.append(GameTreeNode(self.board, self.color, moves_list[i][j], None))
 
     def simulate_random(self):
         '''
         Create a copy of the board and simulate a game with random moves
         '''
         temp_board = deepcopy(self.board)
-        # execute child node's first move
-        temp_board.make_move(self.move, self.color)
 
         # change to opponents color
         temp_color = GameTreeNode.opponent[self.color]
@@ -78,8 +88,8 @@ class GameTreeNode():
             self.wins += 1
         self.win_rate = self.wins / self.visit_count # delete this for UCB
 
-#         if self.parent != None:
-#             self.parent.backpropogate()
+        if self.parent != None:
+            self.parent.backpropogate()
         return
 
 class StudentAI():
@@ -91,11 +101,9 @@ class StudentAI():
         self.board.initialize_game()
         self.opponent = {1:2, 2:1}
         self.color = 2
-        self.time_limit = 15 # seconds per turn
+        self.time_limit = 1 # seconds per turn
 
-    def get_move(self, move):
-        timeout = time.time() + self.time_limit
-
+    def get_move(self, move):            
         # check if opponent gave a turn and execute it
         if len(move) != 0:
             self.board.make_move(move, self.opponent[self.color])
@@ -114,21 +122,65 @@ class StudentAI():
         if len(moves) == 1 and len(moves[0]) == 1:
             self.board.make_move(moves[0][0], self.color)
             return moves[0][0]
-
-        # create all children of root
-        children = list()
-        for i in range(len(moves)):
-            for j in range(len(moves[i])):
-                children.append(GameTreeNode(self.board, self.color, moves[i][j]))
-
+        
+        self.root = GameTreeNode(self.board, self.color, None, None)
+        self.root.create_children()
+        
+        self.mcts()
+        move_chosen = self.best_child()
+        
+        self.board.make_move(move_chosen, self.color)
+        return move_chosen
+    
+    def mcts(self):
+        '''
+        Performs monte carlo tree search for until time runs out
+        '''
+        timeout = time.time() + self.time_limit
+        
         # pick random moves and simulate outcomes
         while time.time() < timeout:
-            i = randint(0, len(children) - 1)
-            children[i].simulate_random()
-
-        # select move with highest win rate
-        sorted_moves = sorted(children, key=lambda x: x.win_rate)
+            i = randint(0, len(self.root.children) - 1)
+            self.root.children[i].simulate_random()
+            
+        return
+    
+    def best_child(self):
+        '''
+        Select move with highest win rate
+        '''
+        sorted_moves = sorted(self.root.children, key=lambda x: x.win_rate)
         print(sorted_moves[-1].visit_count)
         move = sorted_moves[-1].move
-        self.board.make_move(move, self.color)
+        
         return move
+        
+
+# MONTE CARLO TREE SEARCH PSEUDO CODE
+# def monte_carlo_tree_search(root):
+#     while resources_left(time, computational power):
+#         leaf = traverse(root) # leaf = unvisited node 
+#         simulation_result = rollout(leaf)
+#         backpropagate(leaf, simulation_result)
+#     return best_child(root)
+# 
+# def traverse(node):
+#     while fully_expanded(node):
+#         node = best_uct(node)
+#     return pick_univisted(node.children) or node # in case no children are present / node is terminal 
+# 
+# def rollout(node):
+#     while non_terminal(node):
+#         node = rollout_policy(node)
+#     return result(node) 
+# 
+# def rollout_policy(node):
+#     return pick_random(node.children)
+# 
+# def backpropagate(node, result):
+#    if is_root(node) return 
+#    node.stats = update_stats(node, result) 
+#    backpropagate(node.parent)
+# 
+# def best_child(node):
+#     pick child with highest number of visits
